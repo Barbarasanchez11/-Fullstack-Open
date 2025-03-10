2503d4a27b1dca452ccba2601d044a9d2dd463e0
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-03-10 16:49:47 +0100

    js A and B part done

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = (props) => 
+
+{
+  console.log(props)
+return
+<h1>{props.course}</h1>
+}
+
+const Part = (props) => <p>{props.name} {props.exercises}</p>
+
+const Content = (props) => (
+  <div>
+    <Part name={props.part1} exercises={props.exercises1} />
+    <Part name={props.part2} exercises={props.exercises2} />
+    <Part name={props.part3} exercises={props.exercises3} />
+  </div>
+)
+
+const Total = (props) => (
+  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
+
+  return (
+    <div>
+    <Header course={course} />
+    <Content parts={parts} />
+    <Total parts={parts} />
+  </div>
+  )
+}
+
+export default App
