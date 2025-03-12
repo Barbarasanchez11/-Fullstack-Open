e0d5ecf3b3ddb04eec9ef14cc4be5a594b122141
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-03-12 16:10:28 +0100

    part 1C done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+
+
 const Header = (props) => 
 
 {
@@ -20,33 +23,41 @@ const Total = (props) => (
   <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
 )
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
   
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-    <Header course={course} />
-    <Content parts={parts} />
-    <Total parts={parts} />
-  </div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
   )
 }
-
 export default App
